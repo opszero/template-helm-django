dd88da2bd6eb8c9243a5c74bec68f3f41deb758e
Win Htet Aung-3 <winhtet.aung3@frontiir.net>
2023-02-16 19:47:55 +0630

    added /rust path and opszero_rustypy dependency

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import opszero_rustypy
 
 app = Flask(__name__)
 
@@ -6,6 +7,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello World with Python Flask!"
+
+
+@app.route("/rust")
+def rust():
+    return opszero_rustypy.sum_as_string(1, 2)
 
 
 app.run(host="0.0.0.0", port=81)
